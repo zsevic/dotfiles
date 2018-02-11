59e3977ae77ca98ed1dd2b4a0390b79f7a426dc8
zsevic <zsevic95@gmail.com>
2018-02-11 16:37:21 +0100

    :boom: add settings for vscode

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # .files
 
-Content:
+Content
  - `.vimrc`, a config file for Vim text editor
  - `.gitconfig`, a config file for Git CLI
  - `.tmux.conf`, a config file for Tmux
- - `.bash_aliases`, a list of aliases
+ - `.settings.json`, a config file for Visual Studio Code
  - `install.sh`, bash script for installing those files
--- install.sh
+++ install.sh
@@ -7,22 +7,6 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     vim +PluginInstall +qall
 fi
 
-if ! [ -x  "$(command -v zsh)" ]; then
-    echo "installing zsh"
-    sudo apt-get install zsh
-    echo "installing oh-my-zsh"
-    wget -q https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -
-fi
-
-if [ ! -f ~/.oh-my-zsh/custom/themes/bullet-train.zsh-theme ]; then
-    if [ ! -d ~/.oh-my-zsh/custom/themes ]; then
-        echo "making directory for custom themes"
-        mkdir -p ~/.oh-my-zsh/custom/themes
-    fi
-    echo "installing bullet-train theme"
-    wget -q -P ~/.oh-my-zsh/custom/themes "https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme"
-fi
-
 DIR=$(cd "$(dirname "$0")"; pwd)
 
 echo "creating symbolic link ~/.gitconfig from $DIR/git/.config"
@@ -31,9 +15,6 @@ ln -sf $DIR/git/.gitconfig ~/.gitconfig
 echo "creating symbolic link ~/.vimrc from $DIR/vim/.vimrc"
 ln -sf $DIR/vim/.vimrc ~/.vimrc
 
-echo "creating symbolic link ~/.tmux.conf from $DIR/tmux/.tmux.conf"
-ln -sf $DIR/tmux/.tmux.conf ~/.tmux.conf
-
-echo "creating symbolic link ~/.bash_aliases from $DIR/bash/.bash_aliases"
-ln -sf $DIR/bash/.bash_aliases ~/.bash_aliases
-. ~/.bash_aliases
+echo "setting up vscode"
+cp $DIR/vscode/.settings.json /home/zsevic/.config/Code/User/settings.json
+ln -sf $DIR/vscode/.settings.json /home/zsevic/.config/Code/User/settings.json
