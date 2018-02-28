621780fe7f55754b0cc1ed1939120af6f42a2267
zsevic <zsevic95@gmail.com>
2018-02-28 20:59:57 +0100

    :boom: add setup for Antergos

---
+++ antergos_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -S nodejs npm vim fish python-pip ttf-fira-mono prettier tmux mongodb octave
+
+# [install] fisherman
+# fisher omf/theme-bobthefish
+
+# [install] otf-fira-code
+
+npm install -g webpack nodemon yarn claudia standard heroku-cli n
+pip install awscli awsebcli --upgrade --user
+# set -gx PATH ~/.local/bin $PATH // ~/.config/fish/config.fish
