0f42f5d685f6d04d79e921eddf85fd0d7cbe54c1
zsevic <zsevic95@gmail.com>
2019-07-11 23:09:50 +0200

    Add vscode installation

--- minimal_ubuntu_setup.sh
+++ minimal_ubuntu_setup.sh
@@ -27,4 +27,12 @@ sudo add-apt-repository ppa:nathan-renniewaldock/flux
 sudo apt-get update
 sudo apt-get install fluxgui
 
-# install chrome, vscode
+# install chrome
+
+# vscode setup
+sudo apt update
+sudo apt install software-properties-common apt-transport-https
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
