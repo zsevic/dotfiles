6eb6e573e044a2e3de73ca2ea15d4057b0ab8196
zsevic <zsevic95@gmail.com>
2019-07-11 23:00:05 +0200

    Add new setup for Ubuntu

---
+++ minimal_ubuntu_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# node setup
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get install -y nodejs vim curl wget mongodb tmux build-essential git zsh powerline fonts-firacode
+
+# zsh setup
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+echo "exec zsh" >> ~/.bashrc
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+
+# git setup
+ln -sf $DIR/git/.gitconfig ~/.gitconfig
+
+# vim setup
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim -q
+ln -sf $DIR/vim/.vimrc ~/.vimrc
+vim +PluginInstall +qall
+
+# tmux setup
+ln -sf $DIR/tmux/.tmux.conf ~/.tmux.conf
+
+# fluxgui setup
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo apt-get update
+sudo apt-get install fluxgui
+
+# install chrome, vscode
